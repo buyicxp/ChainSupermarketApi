3f3c51bfa26b6c7e2f7c9cf4d2d8216ded5763bd
tanglang <tanglang@47.97.211.33>
2019-12-02 14:02:57 +0800

    测试

--- src/main/java/com/csdj/chainsupermarket/ChainsupermarketApplication.java
+++ src/main/java/com/csdj/chainsupermarket/ChainsupermarketApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ChainsupermarketApplication {
 
     public static void main(String[] args) {
+        System.out.println("2211");
         SpringApplication.run(ChainsupermarketApplication.class, args);
     }
 
