fbd0b055c202aa363ee05a007aa68c9e2115370b
Libbb <55069122+Libbb@users.noreply.github.com>
2019-12-10 16:20:11 +0800

    向仕懿 12月10日 修改BillConcludeController和BillConcludeControllerApi 添加了异常处理

--- src/main/java/com/csdj/chainsupermarket/controller/orderform/BillConcludeController.java
+++ src/main/java/com/csdj/chainsupermarket/controller/orderform/BillConcludeController.java
@@ -2,9 +2,15 @@ package com.csdj.chainsupermarket.controller.orderform;
 
 
 import com.csdj.chainsupermarket.service.imp.orderform.BillConcludeServiceImp;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 后台 调用退款和发货
@@ -18,27 +24,47 @@ public class BillConcludeController {
 
     @Autowired
     BillConcludeServiceImp bcs;
+    Logger logger = LoggerFactory.getLogger(OrderFormController.class);
 
     /**
      * 修改订单状态为退款
      * @param id 订单编号
-     * @return
+     * @return map
      */
     @RequestMapping("refund")
-    public String refund(int id){
-        bcs.update(id);
-        return "";
+    public Map<String, Object> refund(@RequestParam("id")int id){
+        Map<String, Object> result = new HashMap<>();
+        try {
+                bcs.update(id);
+                result.put("msg", "Success");
+                result.put("code", 200);
+                return result;
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
+            result.put("msg", "Server error");
+            result.put("code", 500);
+        }
+        return result;
     }
-
 
     /**
      * 修改订单状态为发货
      * @param id 订单编号
-     * @return
+     * @return map
      */
     @RequestMapping("shipments")
-    public String shipments(int id){
-        bcs.amend(id);
-        return "";
+    public Map<String, Object> shipments(@RequestParam("id")int id){
+        Map<String, Object> result = new HashMap<>();
+        try {
+            bcs.amend(id);
+            result.put("msg", "Success");
+            result.put("code", 200);
+            return result;
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
+            result.put("msg", "Server error");
+            result.put("code", 500);
+        }
+        return result;
     }
 }
--- src/main/java/com/csdj/chainsupermarket/controller/orderform/BillConcludeControllerApi.java
+++ src/main/java/com/csdj/chainsupermarket/controller/orderform/BillConcludeControllerApi.java
@@ -2,9 +2,13 @@ package com.csdj.chainsupermarket.controller.orderform;
 
 import com.csdj.chainsupermarket.entity.orderform.MerchandiseOrderPO;
 import com.csdj.chainsupermarket.service.imp.orderform.BillConcludeServiceImp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author向仕懿
@@ -16,27 +20,47 @@ import javax.annotation.Resource;
 public class BillConcludeControllerApi {
     @Resource
     BillConcludeServiceImp bcs;
+    Logger logger = LoggerFactory.getLogger(OrderFormController.class);
     /**
      * 下单的api接口
      * @param mop 接收的实体类
-     * @return int
+     * @return map
      */
-    @RequestMapping("/order")
-    public int insert(@RequestBody MerchandiseOrderPO mop ){
-        System.out.println(mop.toString()+"=========");
-        int res= bcs.insert(mop);
-        return res;
+    @PostMapping("/order")
+    public Map<String, Object> insert(@RequestBody MerchandiseOrderPO mop ){
+        Map<String, Object> result = new HashMap<>();
+        try {
+            bcs.insert(mop);
+            result.put("msg", "Success");
+            result.put("code", 200);
+            return result;
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
+            result.put("msg", "Server error");
+            result.put("code", 500);
+        }
+        return result;
     }
 
 
     /**
      * 退款的api接口
      * @param id 接收订单编号
-     * @return int
+     * @return map
      */
     @RequestMapping("/refund")
-    public int update(int id){
-        int res=bcs.update(id);
-        return  res;
+    public Map<String, Object> update(@RequestParam("id")int id){
+        Map<String, Object> result = new HashMap<>();
+        try {
+            bcs.update(id);
+            result.put("msg", "Success");
+            result.put("code", 200);
+            return result;
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
+            result.put("msg", "Server error");
+            result.put("code", 500);
+        }
+        return result;
     }
 }
